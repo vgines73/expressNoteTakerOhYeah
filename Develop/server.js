00488a663e1c1ecefe916153e0ac21a717bce3a3
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-12-10 14:24:04 -0800

    api-routes.js is a success.

--- Develop/server.js
+++ Develop/server.js
@@ -2,4 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+const apiRoutes = require("./routes/api-routes");
+
+app.use("/api", apiRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
