488313b51fb8bcab15e89dfbe4da75de00e68a19
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-12-10 14:36:58 -0800

    api-routes.js able to get notes.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const fs = require("fs");
 
-router.get("/", (req, res) => {
-    res.json({ msg: "success"});
+
+router.get("/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        console.log(data);
+        res.json(JSON.parse(data));
+    });
 });
 
 
