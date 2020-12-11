ffd937d6bb1072cc723fc7f7291d9d815636b7d9
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-12-11 14:51:45 -0800

    ISSUE: unable to post data in apiroutes.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -11,8 +11,25 @@ router.get("/notes", (req, res) => {
 });
 
 router.post("/notes", (req, res) => {
-    console.log(req.body);
-    res.send("oh yeah it's working!");
+    //console.log(req.body);
+    //res.send("oh yeah")
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        const ohYeahNotes = JSON.parse(data);
+
+        ohYeahNotes.push({
+            title: req.body.title,
+            text: req.body.text
+        });
+        console.log(ohYeahNotes);
+
+        fs.writeFile("./db/db.json", JSON.stringify(ohYeahNotes)), (err) => {
+            if (err) return res.JSON({ err: "sorry breh didn't add."});
+            return res.json({ msg: "oh yeah!"})
+        }
+
+    })
 });
 
 module.exports = router;
