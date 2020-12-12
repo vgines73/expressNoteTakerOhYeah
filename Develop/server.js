8d97c7ab8325bf0b928ad3e90ea38f1eecac30e2
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-12-11 16:42:09 -0800

    index.html up

--- Develop/server.js
+++ Develop/server.js
@@ -3,11 +3,13 @@ const app = express();
 const PORT = 5000;
 
 const apiRoutes = require("./routes/api-routes");
+const clientRoutes = require("./routes/client-routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api", apiRoutes);
+app.use("/", clientRoutes);
 
 
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
