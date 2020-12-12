196dd5c40488b6b83e09a5d47a91f9704c96719e
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-12-12 13:57:19 -0800

    note.html loads.

--- Develop/routes/client-routes.js
+++ Develop/routes/client-routes.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const path = require("path");
 
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+})
+
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"))
-})
+
 
 module.exports = router;
